54f1f0464bf2aecc3a30720b8eb05fc4f1892a5a
Abhinandan Singh <as5529@srmist.edu.in>
2024-04-11 18:19:03 +0530

    Adding Header

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import Header from "./Header";
 function App() {
-  return;
-  <div>
-    <h1>Hello</h1>
-  </div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+
+function Header() {
+  return (
+    <Header>
+      <h1>QuickNotes</h1>
+    </Header>
+  );
+}
+export default Header;
