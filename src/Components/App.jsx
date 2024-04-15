8a7aebbb5a2dcb2258c2807e23b64330583ddeef
Abhinandan Singh <as5529@srmist.edu.in>
2024-04-15 19:23:33 +0530

    Using Arrow functions to simpilify

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,15 +4,17 @@ import Footer from "./Footer";
 import Note from "./Note";
 import Notes from "../Notes";
 
-function createNotes(noteItem) {
-  return <Note title={noteItem.title} content={noteItem.content} />;
-}
-
 function App() {
   return (
     <div>
       <Header />
-      {Notes.map(createNotes)}
+      {Notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
