1b38b1f35d8a8749728f991a9464da59d985b4b9
Abhinandan Singh <as5529@srmist.edu.in>
2024-04-15 19:13:16 +0530

    Adding functions and looping

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import Notes from "../Notes";
+
+function createNotes(noteItem) {
+  return <Note title={noteItem.title} content={noteItem.content} />;
+}
+
 function App() {
   return (
     <div>
       <Header />
+      {Notes.map(createNotes)}
       <Footer />
-      <Note />
     </div>
   );
 }
